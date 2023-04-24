3c820ccd5c21a1de0f0755c84ef00f677ce8ab2f
Christopher Lozzi <clozzi12@gmail.com>
2023-04-24 13:39:02 -0400

    review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+let currentUser = "Chris"
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const upperCaseCurrentUser = currentUser.toUpperCase();
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+let shortGreeting = `Welcome, ${firstInitial}!`
